0bc0b5e45932d1bb3ac0bdc80678fabbde054bba
25AngelTorres <25AngelTorres@gmail.com>
2018-12-12 22:37:06 -0600

    Create home.php
    inicio de home

---
+++ views/site/home.php
@@ -0,0 +1,16 @@
+<?php
+	//Controlador de vista privada
+	include ('../../controllers/controllerSecurity.php');
+
+	//Header
+	include ('header.php');
+?>
+	
+	<div>
+		home
+	</div>
+	
+<?php
+	//Footer
+	include('footer.php');
+?>
